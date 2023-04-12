d802b0384c4fb7b2965adf43ae357b2caeb33df9
Wandes Cardoso <wandes2030@gmail.com>
2023-04-12 07:26:16 -0300

    New: add timeZone

--- src/Meli.php
+++ src/Meli.php
@@ -30,7 +30,7 @@ final class Meli extends MeliConnector
 
             $this->access_token = $refresh->getAccessToken();
             $this->refresh_token = $refresh->getRefreshToken();
-            $this->expires_in = $refresh->getExpiresAt()->getTimestamp();
+            $this->expires_in = $refresh->getExpiresAt()->setTimezone(new \DateTimeZone($this->timeZone))->getTimestamp();
         }
     }
 }
--- src/Traits/MeliDateTimeImmutableTrait.php
+++ src/Traits/MeliDateTimeImmutableTrait.php
@@ -6,12 +6,19 @@ namespace WandesCardoso\MercadoLivre\Traits;
 
 trait MeliDateTimeImmutableTrait
 {
+    protected string $timeZone = 'America/Sao_Paulo';
+
     protected function convertTimeStamp(?int $timeStamp): ?\DateTimeImmutable
     {
         if (isset($timeStamp)) {
-            return (new \DateTimeImmutable())->setTimestamp($timeStamp);
+            return (new \DateTimeImmutable())->setTimezone(new \DateTimeZone($this->timeZone))->setTimestamp($timeStamp);
         }
 
         return null;
+    }
+
+    protected function setTimezone(string $timeZone): void
+    {
+        $this->timeZone = $timeZone;
     }
 }
